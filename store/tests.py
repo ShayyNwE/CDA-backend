ba9c68e2a7fa15be1c0f614409bdf6dada2b0d79
Julliat Tom <tomjulliat07@gmail.com>
2026-03-20 13:42:02 +0100

    Ajout workflow CI

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import User, Category, Product
 
-# Create your tests here.
+
+class AuthTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user_data = {
+            'email': 'test@bougie.com',
+            'password': 'motdepasse123',
+            'firstname': 'Jean',
+            'lastname': 'Dupont'
+        }
+
+    def test_register(self):
+        response = self.client.post('/api/auth/register/', self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login(self):
+        User.objects.create_user(**self.user_data)
+        response = self.client.post('/api/auth/login/', {
+            'email': 'test@bougie.com',
+            'password': 'motdepasse123'
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+
+class ProductTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.category = Category.objects.create(name='Bougies parfumées')
+
+    def test_liste_produits(self):
+        response = self.client.get('/api/products/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_liste_categories(self):
+        response = self.client.get('/api/categories/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
