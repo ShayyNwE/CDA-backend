3b3358ec0cd64e710e5d01b8c2bcf178f55978f8
Julliat Tom <tomjulliat07@gmail.com>
2026-03-30 14:32:32 +0200

    Corréction lint

--- backend/settings_test.py
+++ backend/settings_test.py
@@ -1,10 +1,35 @@
-from .settings import *
+# settings_test.py
+from .settings import *  # noqa: F401,F403
 
+# Logging simplifié pour tests
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        # Redirige tous tes loggers existants vers console
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'store': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 
-DATABASES['default']['OPTIONS'] = {'sslmode': 'disable'}
-
-LOGGING['handlers'].pop('file', None)
-
+# DB en mémoire pour tests rapides
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
