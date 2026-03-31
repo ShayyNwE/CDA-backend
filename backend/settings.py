76a552e0116b8781467f16973d27729ef8cc8263
Julliat Tom <tomjulliat07@gmail.com>
2026-03-31 16:03:18 +0200

    Modif settings pour prod

--- backend/settings.py
+++ backend/settings.py
@@ -163,6 +163,21 @@ USE_TZ = True
 
 # --- URL / WSGI ---
 ROOT_URLCONF = 'backend.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # --- AUTO FIELD ---
