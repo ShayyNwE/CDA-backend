06b3df09074760ea730f66e93a30cf8473f82b5d
Tom Julliat <146728179+ShayyNwE@users.noreply.github.com>
2026-04-01 12:29:03 +0200

    Pprod (#14)
    * Ajout donnée dans la bdd
    
    * fix bug flake8
    
    * fix bug flake8
    
    * Fix formatting and update instructions in README
    
    * ajout workflow branch staging & PPROD
    
    * ajout workflow branch staging & PPROD

--- seed_data.py
+++ seed_data.py
@@ -1,9 +1,11 @@
 import os
 import django
-from store.models import Category, Product
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
+
+
+from store.models import Category, Product  # noqa: E402
 
 
 def run_seed():
--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 # ✅ Appliquer toutes les migrations
 python manage.py migrate --noinput
 
+python manage.py seed
+
 # ✅ Lancer Gunicorn
 gunicorn backend.wsgi
